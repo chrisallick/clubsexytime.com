18fe057985e193e19f44fe1ef99fffba1859e10f
Chris Allick <chrisallick@gmail.com>
2013-07-19 10:52:18 -0700

    mouse move background image swap and new info drop down

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
-$(document).ready(function() {
+changeBackground = function( x, y ) { 
+	$("#logo .sprite").css({
+		backgroundPositionX: x,
+		backgroundPositionY: y
+	})
+}
 
+var count;
+$(document).ready(function() {
 	$("#info").click(function(event){
 		event.preventDefault();
 		if( $(this).hasClass("hidden") ) {
 			$(this).removeClass("hidden").addClass("show");
-			$("#expanded-info").stop().fadeIn(250);
+			
+			$("#email,#experiments").stop().animate({
+				top: 110
+			}, 150, function(){
+				$("#expanded-info").fadeIn(200);
+			});
 		} else {
 			$(this).removeClass("show").addClass("hidden");
-			$("#expanded-info").stop().fadeOut(250);
+
+			$("#expanded-info").stop().fadeOut(200,function(){
+				$("#email,#experiments").stop().animate({
+					top: 0
+				}, 150);	
+			});
 		}
 	});
 
+	count = 0;
+	$(document).mousemove(function(){
+		//console.log("uuuwwaaaaaaaawaaaaa");
+		count++;
+		if( count > 3 ) {
+			changeBackground( getRandomInt(-1,0)*571, getRandomInt(-1,0)*163);	
+			count = 0;
+		}
+	})
 });
