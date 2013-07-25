e132ad81b0abea5f6c4264057b2977fa8c2914b6
Chris Allick <chrisallick@gmail.com>
2013-07-24 21:05:42 -0700

    fixed timing and html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+changeLogo = function() {
+	clearTimeout( t );
+
+	var $next = $('.logo.on').removeClass('on').next('.logo');
+	if ($next.length) {
+	    $next.addClass('on'); 
+	}
+	else {
+	    $(".logo:first").addClass('on');
+	}
+
+	t = setTimeout(changeLogo, 7000 );
+}
+
 changeBackground = function( x, y ) {
 	$("#logo .sprite").css({
 		backgroundPosition: x+"px "+y+"px",
@@ -5,8 +19,10 @@ changeBackground = function( x, y ) {
 	});
 }
 
-var count;
+var count, t;
 $(document).ready(function() {
+
+	t = setTimeout(changeLogo, 7000 );
 
 	$("#info").click(function(event){
 		event.preventDefault();
