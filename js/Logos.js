6caf30ff9afe9c3da3b6ecd03d8c3fc53df7dd54
Chris Allick <chrisallick@gmail.com>
2013-07-28 22:40:41 -0700

    updated gifs and javasscript

--- js/Logos.js
+++ js/Logos.js
@@ -9,34 +9,34 @@ Logos = function(_prefix,_logos,_onLoad) {
 	this.loaded = 0;
 	this.onLoad = _onLoad;
 
-	this.loadLogos = function() {
-		for( var i = 0, len = self.logos.length; i < len; i++ ) {
-			self.images[i] = new Image();
-			self.images[i].onload = function() {
-				self.loaded++;
-				this.class = "logo";
-				self.images[i] = this;
-				if( self.loaded == self.numLogos ) {
-					self.onLoad();
-				}
-			}
-			self.images[i].src = self.prefix+self.logos[i]+".gif";
-		}
-	}
-
-	this.getLogo = function(index) {
-		if( index !== undefined ) {
-			self.currentLogo = index;
-		} else {
-			var newLogo = getRandomInt(0,self.logos.length-1);
-			while( newLogo == self.currentLogo ) {
-				newLogo = getRandomInt(0,self.logos.length-1);
-			}
-			self.currentLogo = newLogo;
-
-		}
-		return self.images[self.currentLogo];
-	}
-
 	self.loadLogos();
+}
+
+Logos.prototype.loadLogos = function() {
+	for( var i = 0, len = self.logos.length; i < len; i++ ) {
+		self.images[i] = new Image();
+		self.images[i].onload = function() {
+			self.loaded++;
+			this.class = "logo";
+			self.images[i] = this;
+			if( self.loaded == self.numLogos ) {
+				self.onLoad();
+			}
+		}
+		self.images[i].src = self.prefix+self.logos[i]+".gif";
+	}
+}
+
+Logos.prototype.getLogo = function(index) {
+	if( index !== undefined ) {
+		self.currentLogo = index;
+	} else {
+		var newLogo = getRandomInt(0,self.logos.length-1);
+		while( newLogo == self.currentLogo ) {
+			newLogo = getRandomInt(0,self.logos.length-1);
+		}
+		self.currentLogo = newLogo;
+
+	}
+	return self.images[self.currentLogo];
 }
--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(window).load(function(){
 });
 
 var count;
-var logo_images = ["logo","girl","chris"];
+var logo_images = ["logo","girl","chris", "flag"];
 var logos;
 var firstLogo;
 var t;
